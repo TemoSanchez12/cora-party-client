bf83f61e5507ebf34ed83af658c0e41140941bc3
Temo Sanchez <batemo4912@gmail.com>
2024-01-18 11:50:30 -0600

    Setup meta data for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next'
+
 import MainLayout from '../layouts/layout'
+
+export const metadata: Metadata = {
+  title: 'Cora Party - Tu Tienda de Globos, Flores y Arreglos Decorativos',
+  description:
+    '¡Descubre la magia de la celebración con Cora Party! Encuentra una amplia selección de globos, flores y arreglos decorativos para hacer de tus eventos momentos inolvidables. Desde elegantes arreglos florales hasta divertidos globos temáticos, en Cora Party tenemos la combinación perfecta para cada ocasión. Celebra con estilo y crea recuerdos inolvidables con nuestros productos de alta calidad. ¡Haz de cada evento una fiesta inolvidable con Cora Party!',
+}
 
 export default function Home() {
   return (
