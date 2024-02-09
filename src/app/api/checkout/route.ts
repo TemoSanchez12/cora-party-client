cfa447a49a7653a96d8f4b3a5335e351d54486c6
Temo Sanchez <batemo4912@gmail.com>
2024-02-08 21:11:40 -0600

    test payment method with +1 product

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,41 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Stripe } from 'stripe'
 
-// export const POST = async (req: NextRequest) => {
-//   try {
-//     const stripe = new Stripe(process.env.STRIPE_KEY ?? '')
-//     const data = await req.json()
-
-//     const session = await stripe.checkout.sessions.create({
-//       mode: 'payment',
-//       payment_method_types: ['card'],
-//       currency: 'MXN',
-//       line_items: [
-//         {
-//           price_data: {
-//             currency: 'MXN',
-//             unit_amount: 2000,
-//             product_data: {
-//               name: 'Globo de prueba',
-//               description: 'Esto es una prueba',
-//               images: ['https://placehold.co/600x400'],
-//             },
-//           },
-//           quantity: 1,
-//         },
-//       ],
-//       success_url: process.env.BASE_URL + '/resumen-de-compra',
-//     })
-
-//     console.log(session)
-
-//     return NextResponse.json(data)
-//   } catch (err: any) {
-//     console.log(err)
-//     return NextResponse.json({ message: 'Something went wrong' })
-//   }
-// }
-
 export const POST = async (req: NextRequest) => {
   try {
     // Create Checkout Sessions from body params.
@@ -48,7 +13,31 @@ export const POST = async (req: NextRequest) => {
             currency: 'MXN',
             unit_amount: 2000,
             product_data: {
-              name: 'Globo de prueba',
+              name: 'Globo de prueba 1',
+              description: 'Esto es una prueba',
+              images: ['https://placehold.co/600x400'],
+            },
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: 'MXN',
+            unit_amount: 3000,
+            product_data: {
+              name: 'Globo de prueba 2',
+              description: 'Esto es una prueba',
+              images: ['https://placehold.co/600x400'],
+            },
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: 'MXN',
+            unit_amount: 3000,
+            product_data: {
+              name: 'Globo de prueba 3',
               description: 'Esto es una prueba',
               images: ['https://placehold.co/600x400'],
             },
